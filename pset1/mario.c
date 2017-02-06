a2719d5cf97588158f054b2aff4a697e9bfdfa82
Bilinskij Yaroslav <bilinskij.yaroslav@gmail.com>
2017-02-06 14:01:20 +0000

    added comments

--- pset1/mario.c
+++ pset1/mario.c
@@ -2,33 +2,42 @@
 #include <stdio.h>
 
 int main(void) {
+    const int max_height = 23;
     int height = 0;
     int sharps = 0;
     int spaces = 0;
     
+    //Проверяем на правильность ввода.
     do {
         printf("Height: ");
         height = get_int();
-    } while (height < 0 || height > 23);
+    } while (height < 0 || height > max_height);
     
+    //Вывод строк пирамидки
     for (int i=0; i<height; i++) {
-
+        //Вычисление необходимого кол-ва пробелов и решоток для текущей строки.
         spaces = height - i - 1; 
         sharps = height - spaces;
         
+        //Вывод пробелов перед решотками
         for (int x=0; x < spaces; x++) {
             printf(" ");
         }
+        
+        //Вывод решоток первой пирамидки
         for (int x=0; x < sharps; x++) {
             printf("#");
         }
         
-        printf("  ");
+        //Два прорбела между пирамидками
+        printf("  "); 
         
+        //Вывод решоток второй пирамидки
         for (int x=0; x < sharps; x++) {
             printf("#");
         }
         
+        //Перенос на следущую строку
         printf("\n");
         
     }
