66070286843e9e93aac832e2d25d1527421cf801
Bilinskij Yaroslav <bilinskij.yaroslav@gmail.com>
2017-02-08 09:30:20 +0000

    after decomposition

--- pset1/mario.c
+++ pset1/mario.c
@@ -1,45 +1,59 @@
 #include <cs50.h>
 #include <stdio.h>
+//Константы
+const int MIN_HEIGHT = 0;
+const int MAX_HEIGHT = 23;
+
+int getHeight(); //Запрос ввода высоты
+void showRow(); //Вывести строку пирамидки в зависимости от номера и высоты
+void showPyramid(); //Вывести пирамидку.
 
 int main(void) {
-    const int max_height = 23;
+    showPyramid(getHeight()); //Делаем все хорошо, но увы с переменными.
+    return 0;
+}
+
+int getHeight() {
     int height = 0;
-    int sharps = 0;
-    int spaces = 0;
-    
-    //Проверяем на правильность ввода.
     do {
         printf("Height: ");
         height = get_int();
-    } while (height < 0 || height > max_height);
+    } while (height < MIN_HEIGHT || height > MAX_HEIGHT); //Проверка вваода по условию
+    return height;
+} 
+
+void showRow(int number, int height) {
+    int sharps = 0;
+    int spaces = 0;
     
-    //Вывод строк пирамидки
-    for (int i=0; i<height; i++) {
-        //Вычисление необходимого кол-ва пробелов и решоток для текущей строки.
-        spaces = height - i - 1; 
-        sharps = height - spaces;
-        
-        //Вывод пробелов перед решотками
-        for (int x=0; x < spaces; x++) {
-            printf(" ");
-        }
-        
-        //Вывод решоток первой пирамидки
-        for (int x=0; x < sharps; x++) {
-            printf("#");
-        }
-        
-        //Два прорбела между пирамидками
-        printf("  "); 
-        
-        //Вывод решоток второй пирамидки
-        for (int x=0; x < sharps; x++) {
-            printf("#");
-        }
-        
-        //Перенос на следущую строку
-        printf("\n");
-        
+    //Вычисление необходимого кол-ва пробелов и решоток для строки.
+    spaces = height - number - 1; 
+    sharps = height - spaces;
+    
+    //Вывод пробелов перед решотками
+    for (int x=0; x < spaces; x++) {
+        printf(" ");
     }
-    return 0;
+        
+    //Вывод решоток первой половины пирамидки
+    for (int x=0; x < sharps; x++) {
+        printf("#");
+    }
+    
+    //Два прорбела между половинками
+    printf("  "); 
+    
+    //Вывод решоток второй половины пирамидки
+    for (int x=0; x < sharps; x++) {
+        printf("#");
+    }
+    
+    //Перенос на следущую строку
+    printf("\n");
 }
+
+void showPyramid(int height) {
+    for (int i = 0; i<height; i++) {
+        showRow(i,height);
+    }
+} 
