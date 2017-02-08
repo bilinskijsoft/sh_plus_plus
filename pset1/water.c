2575f18cf2c132318838731caa5021c7a1908399
Bilinskij Yaroslav <bilinskij.yaroslav@gmail.com>
2017-02-08 08:50:26 +0000

    after decomposition

--- pset1/water.c
+++ pset1/water.c
@@ -1,8 +1,28 @@
 #include <cs50.h>
 #include <stdio.h>
 
+//Константы
+const int WHATER_CONSUMPTION_PER_MINUTE = 12;
+
+//Функции
+int getMinutes(); //Запрос ввода минут
+int getBottlesNumber(); //Получение количества бутылок в зависимости от времени и расхода.
+void showBottles(); //Вывоб еличества бутылок
+
 int main(void) {
-    printf("Water: "); //Запрос ввода
-    int minutes = get_int();
-    printf("Bottles: %i\n",minutes*12); //Вывод результата
+    showBottles(getBottlesNumber(getMinutes())); //Делаем все хорошо, и утопично! Без переменных.
+    return 0;
+}
+
+int getMinutes() {
+    printf("Minutes: ");
+    return get_int();
+}
+
+int getBottlesNumber(int minutes) {
+    return WHATER_CONSUMPTION_PER_MINUTE * minutes;
+}
+
+void showBottles(int count) {
+    printf("Bottles: %i\n",count);
 }
