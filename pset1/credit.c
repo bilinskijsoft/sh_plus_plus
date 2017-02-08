fbeff6a9f7907d106e53651b956ae4ad82b311ed
Bilinskij Yaroslav <bilinskij.yaroslav@gmail.com>
2017-02-08 09:38:58 +0000

    after decomposition

--- pset1/credit.c
+++ pset1/credit.c
@@ -1,38 +1,71 @@
 #include <cs50.h>
 #include <stdio.h>
 
-bool card_check(long long number);
+long long checkCardNumber(); //Проверка валидности номера карты
+long long getCardNumber(); //Запрос на ввод номера карты
+int getCardType(); //Получить тип карты. Возвращает 1 - AMEX, 2 - Visa, 3 - MasterCard, 0 - Неправильно.
+void showCardType(); //Вывести тип карты
 
 int main(void) {
-    //Запрос номера карты
-    printf("Number: ");
-    long long card_number = get_long_long();
-    
-    if (card_number < 1000000000000000 && card_number > 99999999999999 && card_check(card_number)) { //AMEX
-        printf("AMEX\n");
-    }
-    else if (card_number < 10000000000000000 && card_check(card_number)){ //Visa или MasterCard
-        if (card_number / 1000000000000000 == 4) { //Visa
-            printf("VISA\n");
-        } 
-        //MasterCard
-        else if(card_number / 100000000000000 == 51 || card_number / 100000000000000 == 52 || card_number / 100000000000000 == 53 || card_number / 100000000000000 == 54 || card_number / 100000000000000 == 55){
-            printf ("MASTERCARD\n");   
-        }
-        else {
-            printf("INVALID\n");
-        }
-    }
-    else {
-        printf("INVALID\n");
-    }
+    showCardType(getCardType(checkCardNumber(getCardNumber()))); //Делаем все хорошо.
     return 0;
 }
 
-bool card_check(long long number) {
-    long long tmp_number = number;
+void showCardType(int type) {
+    switch (type) {
+        case 0:
+            printf("INVALID\n");
+            break;
+        case 1:
+            printf("AMEX\n");
+            break;
+        case 2:
+            printf("VISA\n");
+            break;
+        case 3:
+            printf("MASTERCARD\n");
+    }    
+}
+
+int getCardType(long long number) {
+    switch (number / 100000000000000) { //Проверяем первые цыфры номера
+        case 3: //AMEX
+            return 1; 
+            break;
+        case 40: //Visa
+        case 41:
+        case 42:
+        case 43:
+        case 44:
+        case 45:
+        case 46: 
+        case 47:
+        case 48:
+        case 49:
+            return 2;
+            break;
+        case 51: //MasterCard
+        case 52:
+        case 53:
+        case 54:
+        case 55:
+            return 3;
+            break;
+        default:
+            return 0;
+            break;
+    }   
+}
+
+long long getCardNumber() {
+    printf("Number: ");
+    return get_long_long();
+}
+
+long long checkCardNumber(long long number) {
+    long long tmp_number = number;  //Создаем временную переменную для номера карты
     int num_count = 0;
-    int sum = 0;
+    int sum = 0; //Сумма для проверки.
     
     do { //Перебираем все цыфры номера карты
         num_count++;
@@ -53,8 +86,8 @@ bool card_check(long long number) {
     } while (tmp_number > 0);
     
     if (sum % 10 == 0) {//Если поседняя цыфра суммы 0 тогда номер карты правильный
-        return true;
+        return number;
     }
     
-    return false;
+    return 0;
 }
